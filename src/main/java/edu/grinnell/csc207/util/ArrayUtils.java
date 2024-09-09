199d983196580d1edd24e7930a4a6de69a1b7066
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2024-09-08 19:24:37 -0500

    Add a few more sample files.

---
+++ src/main/java/edu/grinnell/csc207/util/ArrayUtils.java
@@ -0,0 +1,23 @@
+package edu.grinnell.csc207.util;
+
+/**
+ * Various array utilities.
+ *
+ * @author YOUR NAME HERE.
+ */
+public class ArrayUtils {
+  /**
+   * Find the minimum value in a non-empty array.
+   *
+   * @param vals
+   *   The array of values
+   *
+   * @return
+   *   The minimum value
+   */
+  public static int min(int[] vals) {
+    return 0;
+  } // min(int[])
+
+} // class ArrayUtils
+
